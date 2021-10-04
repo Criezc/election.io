aa77fe0d1390a711ae5e06434c539950355e162b
friscolie <friscolieyanto@gmail.com>
2021-10-04 20:46:21 +0700

    commit v1

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ["./**/*.html"],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        dark: "#0C0217",
+        lightBlue: "#00FFF0",
+      },
+      backgroundImage: {
+        "hero-pattern": "url('/img/BACKGROUND.PNG')",
+      },
+      fontFamily: {
+        monts: ["Montserrat", "ui-sans-serif"],
+      },
+      borderWidth: {
+        1: "1px",
+      },
+      screens: {
+        "3xl": "1980px",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
